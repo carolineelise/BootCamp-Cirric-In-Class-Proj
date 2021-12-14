6a8f7600beb7fd6424fb311b5ca20437efd89d1d
carolineelise <90719451+carolineelise@users.noreply.github.com>
2021-12-14 14:30:12 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BootCampCirriculum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gitgithub = new Topic();
+            gitgithub.Name = "Git/Github";
+            gitgithub.Category = "SCM";
+            gitgithub.Difficulty = 2 ;
+            gitgithub.Print();
+
+            var sqlserver = new Topic();
+            { Name = sqlserver
+                    } 
+        }
+        4 sides quadrilatteral in any measurement you choose
+
+    }
+}
+GITHUB.COM/GPDOUD
